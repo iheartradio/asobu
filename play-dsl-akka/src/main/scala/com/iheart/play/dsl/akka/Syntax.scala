7f4fdaa7408600037c123b6759977cfd73c1355d
Kailuo Wang <kailuo.wang@gmail.com>
2015-11-23 17:12:11 -0500

    format

--- play-dsl-akka/src/main/scala/com/iheart/play/dsl/akka/Syntax.scala
+++ play-dsl-akka/src/main/scala/com/iheart/play/dsl/akka/Syntax.scala
@@ -1,6 +1,6 @@
 package com.iheart.play.dsl.akka
 
-import akka.actor.{ActorSelection, ActorRef}
+import akka.actor.{ ActorSelection, ActorRef }
 import akka.util.Timeout
 import akka.pattern.ask
 import com.iheart.play.dsl.Syntax._
@@ -9,7 +9,7 @@ object Syntax {
   implicit def actorAskBuilder(implicit to: Timeout) = new AskableBuilder[ActorRef] {
     def apply(t: ActorRef): Askable = t.ask
   }
-   implicit def actorSelectionAskBuilder(implicit to: Timeout) = new AskableBuilder[ActorSelection] {
+  implicit def actorSelectionAskBuilder(implicit to: Timeout) = new AskableBuilder[ActorSelection] {
     def apply(t: ActorSelection): Askable = t.ask
   }
 }
